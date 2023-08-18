3f89f0758008f40f31ec0faf2a855b9e6ec77f5e
worldwalker2000 <space3334@gmail.com>
2023-08-17 20:44:01 -0700

    it works lol

---
+++ README.md
@@ -0,0 +1,21 @@
+# gogrep
+
+This is a very simple grep command implemented in go. I did this as a project
+to learn go but it turned out to be way to easy.
+
+It uses goroutines to speed up grepping.
+
+```shell
+grep -rn "include" > /dev/null  0.34s user 0.03s system 99% cpu 0.373 total
+~/code/gogrep/gogrep -rn "include" > /dev/null  0.10s user 0.13s system 356% cpu 0.067 total
+```
+
+It is actually faster.
+
+This is not even close to a full grep as only the folling kinds of commands work
+
+```shell
+gogrep "pattern" file
+gogrep -n "pattern" file
+gogrep -rn "pattern"
+```
--- main.go
+++ main.go
@@ -1,7 +1,99 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "flag"
+    "os"
+    "regexp"
+    "bufio"
+    "path/filepath"
+    "sync"
+)
+
+func parseFlags() (bool, bool) {
+    r := flag.Bool("r", false, "recursive")
+    n := flag.Bool("n", false, "show line numbers")
+
+    rn := flag.Bool("rn", false, "show line numbers and go recursive")
+    nr := flag.Bool("nr", false, "show line numbers and go recursive")
+
+    flag.Parse()
+
+    if *rn || *nr {
+        *r, *n = true, true
+    }
+
+    return *r, *n
+}
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
+func grepFile(re *regexp.Regexp, name string, n bool, full bool) {
+    f, err := os.Open(name)
+    check(err)
+
+    scanner := bufio.NewScanner(f)
+
+    i := 1
+    for scanner.Scan() {
+        line := scanner.Text()
+
+        if re.MatchString(line) {
+            if n {
+                if full {
+                    fmt.Printf("./%s:%d:%s\n", name, i, line)
+                } else {
+                    fmt.Printf("%d:%s\n", i, line)
+                }
+            } else {
+                fmt.Printf("%s\n", line)
+            }
+        }
+
+        i++
+    }
+}
+
+// grep "pattern" file
+// grep -rn "pattern"
 
 func main() {
-    fmt.Println("Hello, World!")
+    r, n := parseFlags()
+
+    pattern := flag.Arg(0)
+
+    re, err := regexp.Compile(pattern)
+    check(err)
+
+    file := flag.Arg(1)
+
+    if file != "" && r {
+        panic("Can't specify a file and recursive.")
+    }
+
+    if r {
+        var wg sync.WaitGroup
+
+        filepath.Walk(".", func(p string, info os.FileInfo, err error) error {
+            if err != nil {
+                return err
+            }
+
+            wg.Add(1)
+            go func() {
+                defer wg.Done()
+                grepFile(re, p, n, true)
+            }()
+
+            return nil
+        })
+
+        wg.Wait()
+    } else {
+        grepFile(re, file, n, false)
+    }
 }
